55c6e031669342cac43fa0dcc0ecff7a3f042c28
zackstephens <zackstephens99@gmail.com>
2024-11-21 14:51:19 -0800

    Message class
    Added message class, messageType enum

---
+++ src/main/java/ParkingGarage/Client.java
@@ -0,0 +1,5 @@
+package ParkingGarage;
+
+public class Client {
+    
+}
---
+++ src/main/java/ParkingGarage/MessageType.java
@@ -0,0 +1,10 @@
+package ParkingGarage;
+
+public enum MessageType {
+    LOGIN,
+    ENTER,  
+    EXIT,   
+    REPORT,
+    ERROR,
+    // add new types when thought of
+}
