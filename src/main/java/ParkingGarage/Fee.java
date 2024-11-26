25bcde2a15bccb2c4446a95da81306f5d0278ba0
MajiPearson <majipearson@gmail.com>
2024-11-25 19:01:27 -0800

    Updated Fee and Garage classes
    - Fee class: Added save and load methods, and extended to DataLoaderable.
    - Garage class: Added save and load methods, and extended to DataLoaderable.

--- src/main/java/ParkingGarage/Fee.java
+++ src/main/java/ParkingGarage/Fee.java
@@ -2,22 +2,29 @@ package ParkingGarage;
 
 import org.json.JSONObject;
 
-public class Fee {
+public class Fee extends DataLoaderable {
 	private int id;
 	private FeeType type;
 	private int cost;
 	
+	// public constructor
 	public Fee(FeeType type, int cost) {
+		if (cost < 0) {
+			throw new IllegalArgumentException("Cost cannot be negative.");
+		}
 		this.type = type;
 		this.cost = cost;
 	}
 	
+	// private constructor
 	private Fee(int id, FeeType type, int cost) {
 		this.id = id;
 		this.type = type;
 		this.cost = cost;
 	}
 	
+	
+	// Getters
 	public int getId() {
 		return id;
 	}
@@ -30,7 +37,39 @@ public class Fee {
 		return cost;
 	}
 	
+	// Static method to load a fee by ID
+	public static Fee load(int id) {
+		try {
+			DataLoader dataLoader = new DataLoader();
+			JSONObject feeJson = dataLoader.getJSONObject("Fee_" + id);
+			return load(feeJson);
+		} catch (Exception e) {
+			System.err.println("Failed to load Fee with ID: " + id);
+			e.printStackTrace();
+			return null;
+		}
 
+	}
+	
+	// Static method to load a fee from a JSON object
+	public static Fee load(JSONObject object) {
+		int id = object.getInt("id");
+		FeeType type = FeeType.valueOf(object.getString("type"));
+		int  cost = object.getInt("cost");
+		return new Fee(id, type, cost);
+	}
+	
+	// Save fee data to DataLoader
+	@Override
+	public void save() {
+		JSONObject feeJson = new JSONObject();
+		feeJson.put("id", id);
+		feeJson.put("type", type.name());
+		feeJson.put("cost", cost);
+		DataLoader dataLoader = new DataLoader();
+		dataLoader.put("Fee_" + id, feeJson);
+		dataLoader.saveData();
+	}
 	
 	@Override
 	public String toString() {
--- src/main/java/ParkingGarage/Garage.java
+++ src/main/java/ParkingGarage/Garage.java
@@ -1,7 +1,7 @@
 package ParkingGarage;
 import org.json.JSONObject;
 
-public class Garage {
+public class Garage extends DataLoaderable {
 	private int id;
 	private String name;
 	private String address;
@@ -9,12 +9,14 @@ public class Garage {
 	private int occupiedSpaces;
 	private int totalSpaces;
 	
+	// public constructor
 	public Garage(String name, String address, int totalSpaces) {
 		this.name = name;
 		this.address = address;
 		this.totalSpaces = totalSpaces;
 	}
 	
+	// private constructor
 	private Garage(int id, String name, String address, Fee currentParkingFee, int totalSpaces) {
 		this.id = id;
 		this.name = name;
@@ -57,15 +59,19 @@ public class Garage {
 		this.name = name;
 	}
 	
-	public void setParkingFee(Fee parkingFee) {
-		this.currentParkingFee = parkingFee;
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public void setParkingFee(Fee fee) {
+		this.currentParkingFee = fee;
 	}
 	
 	public void setTotalSpaces(int totalSpaces) {
 		this.totalSpaces = totalSpaces;
 	}
 	
-	// Decrement available spaces
+	// update occupied spaces
 	public void decrementAvailableSpaces() {
 		if (occupiedSpaces < totalSpaces) {
 			occupiedSpaces++;
@@ -83,13 +89,40 @@ public class Garage {
 		}
 	}
 	
-	// Load garage details from an ID
-	public void load(int id) {
-		
+	// static method to load garage by ID
+	public static Garage load(int id) {
+		DataLoader dataLoader = new DataLoader();
+		JSONObject garageJson = dataLoader.getJSONObject("Garage_" + id);
+		return load(garageJson);
 	}
 	
-	// Load garage from JSON
-	public JSONObject save() {
-		
+	// static method to load garage from JSON object
+	public static Garage load(JSONObject object) {
+		try {
+			int id = object.getInt("id");
+			String name = object.getString("name");
+			String address = object.getString("address");
+			Fee fee = Fee.load(object.getJSONObject("currentParkingFee"));
+			int totalSpaces = object.getInt("totalSpaces");
+			return new Garage(id, name, address, fee, totalSpaces);
+		} catch (Exception e) {
+			System.err.println("Error loading Garage from JSON: " + e.getMessage());
+			return null;
+		}
 	}
+	
+	// Save garage data to DataLoader
+    @Override
+    public void save() {
+        JSONObject garageJson = new JSONObject();
+        garageJson.put("id", id);
+        garageJson.put("name", name);
+        garageJson.put("address", address);
+        garageJson.put("totalSpaces", totalSpaces);
+        garageJson.put("occupiedSpaces", occupiedSpaces);
+        garageJson.put("currentParkingFee", currentParkingFee);
+        DataLoader dataLoader = new DataLoader();
+        dataLoader.put("Garage_" + id, garageJson);
+        dataLoader.saveData();
+    }
 }
